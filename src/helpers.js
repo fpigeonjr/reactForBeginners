072c436dbbd8aa646e50ca57de5833931c2522c3
Frank Pigeon Jr <frank.pigeonjr@gmail.com>
2017-08-04 23:28:22 -0500

    adds hipster ipsum to helpers

--- src/helpers.js
+++ src/helpers.js
@@ -16,9 +16,9 @@ export function slugify(text) {
 }
 
 export function getFunName() {
-  const adjectives = ['adorable', 'beautiful', 'clean', 'drab', 'elegant', 'fancy', 'glamorous', 'handsome', 'long', 'magnificent', 'old-fashioned', 'plain', 'quaint', 'sparkling', 'ugliest', 'unsightly', 'angry', 'bewildered', 'clumsy', 'defeated', 'embarrassed', 'fierce', 'grumpy', 'helpless', 'itchy', 'jealous', 'lazy', 'mysterious', 'nervous', 'obnoxious', 'panicky', 'repulsive', 'scary', 'thoughtless', 'uptight', 'worried'];
+  const adjectives = ['adorable', 'beautiful', 'clean', 'drab', 'elegant', 'fancy', 'glamorous', 'sustainable', 'long', 'magnificent', 'old-fashioned', 'plain', 'quaint', 'sparkling', 'ugliest', 'unsightly', 'angry', 'chic', 'clumsy', 'defeated', 'embarrassed', 'fierce', 'grumpy', 'helpless', 'itchy', 'jealous', 'lazy', 'sriracha', 'nervous', 'obnoxious', 'panicky', 'repulsive', 'scary', 'thoughtless', 'uptight', 'worried', 'fugly', 'smelly', 'tight', 'asymmetrical', 'lofi', 'retro', 'shabby', 'normcore'];
 
-  const nouns = ['women', 'men', 'children', 'teeth', 'feet', 'people', 'leaves', 'mice', 'geese', 'halves', 'knives', 'wives', 'lives', 'elves', 'loaves', 'potatoes', 'tomatoes', 'cacti', 'foci', 'fungi', 'nuclei', 'syllabuses', 'analyses', 'diagnoses', 'oases', 'theses', 'crises', 'phenomena', 'criteria', 'data'];
+  const nouns = ['women', 'men', 'unicorn', 'chicharrones', 'feet', 'peeps', 'leaves', 'hammock', 'mustache', 'crucifix', 'keytar', 'wives', 'skateboard', 'elves', 'loaves', 'mixtape', 'tomatoes', 'cacti', 'meggings', 'fungi', 'nuclei', 'syllabuses', 'analyses', 'axe', 'kickstarter', 'dreamcatcher', 'crises', 'phenomena', 'criteria', 'data', 'momma', 'dwarfes', 'boots', 'man-bun', 'chillwave', 'vegan', 'cornhole', 'flannel', 'leggings', 'pug', 'cronut'];
 
   return `${rando(adjectives)}-${rando(adjectives)}-${rando(nouns)}`;
 }
